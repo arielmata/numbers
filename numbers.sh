bda5773672d2974634c985076b68509c9ae773be
Ariel Gutierrez <arigutierrez@chapman.edu>
2020-11-17 17:13:38 -0800

    Adds numbers.sh

--- numbers.sh
+++ numbers.sh
@@ -7,12 +7,11 @@ read NUMBER
 N=1
 while [ $NUMBER -ge $N ]
 do
-	if [ $((N%2)) -eq 0]
+	if [ $((N%2)) -eq 0 ]
 	then
 		echo "$N Even"
 	else
 		echo "$N Odd"
 	fi
-	N=$(N+1))
+	N=$((N+1))
 done
-echo "Thank you"
